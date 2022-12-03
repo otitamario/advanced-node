280667cb65ee2b7d28957c7d2ba47dde18feae85
Mario S J <mario2013sj@gmail.com>
2022-12-03 01:03:13 -0400

    finish typescript config

--- src/main/api/index.ts
+++
@@ -1,5 +0,0 @@
-import '../config/module-alias'
-import { PersonController } from "@/application/controllers/person"
-const p = new PersonController()
-console.log(p.speak('mario'))
-console.log(p.speak())
